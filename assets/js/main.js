66e9aad53ebab74da811703365048c909ff7cae4
jaimeHMol <jaimehmol@yahoo.com>
2019-08-20 22:41:17 -0300

    Changing the footer size. Adding some credits on comments.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,12 @@
 /*
   Base on code from:
+  
   Read Only by HTML5 UP
 	html5up.net | @ajlkn
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+
+  Using the lightweight JavaScript library for creating particles developed
+  by VincentGarreau. published on: https://github.com/VincentGarreau/particles.js
 */
 
 /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
